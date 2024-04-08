517e1ab3998b9f57dc016a717cd24b84db72dd0b
lover-of-the-eclipse <zhang.miles@charterschool.org>
2024-04-07 21:44:50 -0400

    actual content I hope

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ anime({
     easing: 'easeInOutCubic',
     duration: 3000
 })
+
+anime({
+    targets: '.title',
+    easing: 'easeInOutCubic',
+    duration: 3000,
+    keyframes: [
+        {translateY: -400, duration: 0},
+        {translateY: 0, duration: 3000}
+    ],
+    opacity: [
+        { value: "0%", duration: 0},
+        { value: "0%", duration: 1400},
+        { value: "100%", duration: 1600},
+    ],
+    delay: 500
+})
+
+anime({
+    targets: '.button-container .claim-button',
+    easing: 'easeInOutCubic',
+    keyframes: [
+        {translateX: -400, duration: 0},
+        {translateX: 0, duration: 2000}
+    ],
+    opacity: [
+        { value: 0, duration: 0},
+        { value: 0, duration: 200},
+        { value: 1, duration: 1600}
+    ],
+    delay: anime.stagger(200, {start: 2500}),
+})
